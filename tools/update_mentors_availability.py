da67dac728bdd1b65f4f25fd674e37cb6dd5120d
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2024-04-13 18:12:15 +0200

    Load yml and find mentors by name with test

---
+++ tools/update_mentors_availability.py
@@ -0,0 +1,33 @@
+import yaml
+
+
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+
+def update_yaml(file_path, new_data):
+    with open(file_path, 'w') as file:
+        yaml.dump(new_data, file)
+
+
+def find_mentor_by_name(name, mentors):
+    for mentor in mentors:
+        if mentor.get('name') == name:
+            return mentor
+    return None
+
+
+def find_mentors_by_names(names, mentors):
+    found_mentors = []
+    for mentor in mentors:
+        if mentor.get('name') in names:
+            found_mentors.append(mentor)
+
+    return found_mentors
+
+
+if __name__ == "__main__":
+    yaml_file_path = '/Users/dricazenck/workspace/london/_data/mentors.yml'
+    data = load_yaml(yaml_file_path)
+    print(len(data))
---
+++ tools/update_mentors_availability_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from update_mentors_availability import *
+
+
+class TestUpdateMentorsAutomation(unittest.TestCase):
+
+    def setUp(self):
+        self.data = load_yaml("../_data/mentors.yml")
+
+    def test_load_file(self):
+        self.assertEqual(89, len(self.data))
+
+    def test_find_existing_item(self):
+        mentor_name = "Rajani Rao"
+        mentor = find_mentor_by_name(mentor_name, self.data)
+        self.assertIsNotNone(mentor)
+        self.assertEqual(mentor['name'], mentor_name)
+
+    def test_find_non_existing_item(self):
+        non_existing_item = find_mentor_by_name("Adriana Maria", self.data)
+        self.assertIsNone(non_existing_item)
+
+    def test_found_mentors_by_name(self):
+        found_mentors = find_mentors_by_names(["Rajani Rao", "Eleonora Belova"], self.data)
+        self.assertEqual(2, len(found_mentors))
+
+
+if __name__ == '__main__':
+    unittest.main()
